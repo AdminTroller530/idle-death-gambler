5f89e455c7374bc3b970cf4c2c1b5b9aff7ddec2
AdminTroller <lauedmondyc@gmail.com>
2026-02-03 22:28:17 +0800

    basic movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Vector2 move;
+    float speed = 12f;
+    Rigidbody2D rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        move = context.ReadValue<Vector2>().normalized;
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = move * speed;
+    }
+}
