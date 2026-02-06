545c97ed24035050f1ccfe40b307b231696c2e2b
AdminTroller <lauedmondyc@gmail.com>
2026-02-07 03:06:00 +0800

    bullet collides with wall

--- Assets/Scripts/PlayerBullet.cs
+++ Assets/Scripts/PlayerBullet.cs
@@ -4,6 +4,12 @@ public class PlayerBullet : MonoBehaviour
 {
     float speed = 16f;
     float lifetime = 3f;
+    BoxCollider2D col;
+
+    void Awake()
+    {
+        col = gameObject.GetComponent<BoxCollider2D>();
+    }
 
     void Update()
     {
@@ -11,5 +17,13 @@ public class PlayerBullet : MonoBehaviour
 
         lifetime -= Time.deltaTime;
         if (lifetime <= 0) Destroy(gameObject);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Wall")
+        {
+            Destroy(gameObject);
+        }
     }
 }
