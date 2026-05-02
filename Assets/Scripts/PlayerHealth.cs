17cb9d8af51e74325ce5098210331870e9784202
AdminTroller <lauedmondyc@gmail.com>
2026-05-03 04:53:09 +0800

    simple i-frame system

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -6,10 +6,24 @@ public class PlayerHealth : MonoBehaviour
     float maxHealth = 40f;
     float health = 40f;
     [SerializeField] TextMeshProUGUI healthText; // temp
+    SpriteRenderer spriteRenderer;
+
+    float maxInvincibleTimer = 1.1f, invincibleTimer = 0; // I-frames
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     void Update()
     {
         healthText.text = $"{(int)health}/{(int)maxHealth}";
+
+        if (invincibleTimer > 0) {
+            invincibleTimer -= Time.deltaTime;
+            spriteRenderer.color = new Color(1,1,1,0.6f);
+        }
+        else spriteRenderer.color = new Color(1,1,1,1);
     }
 
     public void Heal(float heal)
@@ -20,12 +34,15 @@ public class PlayerHealth : MonoBehaviour
 
     public void TakeDamage(float damage)
     {
+        if (invincibleTimer > 0) return;
+
         health -= damage;
         Debug.Log("Took " + damage + " damage! Health Left: " + health);
         if (health <= 0)
         {
             Death();
         }
+        invincibleTimer = maxInvincibleTimer;
     }
 
     void Death()
