c6d8a2e281f1309925ff1ec4cdd2a54b7fe45580
AdminTroller <lauedmondyc@gmail.com>
2026-05-07 19:33:33 +0800

    basic room generating algorithm works

--- Assets/Scripts/RoomGenerator.cs
+++ Assets/Scripts/RoomGenerator.cs
@@ -6,39 +6,58 @@ public class RoomGenerator : MonoBehaviour
 
     // room group named by where their exits are
     [SerializeField] GameObject[] UD, UL, UR, DL, DR, LR;
+    int[] roomExits = {-1, 0, 2, 1, 2, 2}; // up: 0, down: 1, right: 2
     GameObject[][] rooms;
     [SerializeField] GameObject[] hallsUD, hallsLR;
 
-    int[][] exitMapping = { // top to bottom: up, down, right - exit can go to what room (index)?
+
+    int previousExit = 2; // up: 0, down: 1, right: 2
+    int[][] exitMapping = { // up, down, right - previous exit can lead to what rooms?
         new int[]{0, 4},
         new int[]{0, 2},
         new int[]{1, 3, 5}
     };
-    int previousExit = 2; // up, down, right
 
     Vector2 roomDims = new Vector2(20, 20); // dimensions of rooms (width, height)
     int hallLength = 10;
-    Vector2 roomPos; // position of next room to spawn
+    Vector2 currentPos = new Vector2(0, 0); // position of current room
 
     void Start()
     {
-        roomPos = new Vector2(roomDims.x + hallLength, 0);
         rooms = new GameObject[][]{UD, UL, UR, DL, DR, LR};
 
-        SpawnRoom();
+        SpawnNextRoom();
+        SpawnNextRoom();
+        SpawnNextRoom();
     }
 
-    void SpawnRoom()
+    public void SpawnNextRoom()
     {
-        if (previousExit == 0) Instantiate(hallsUD[0], roomPos - (roomDims.y + hallLength)*0.5f * Vector2.up, transform.rotation, tileGrid);
-        else if (previousExit == 1) Instantiate(hallsUD[0], roomPos + (roomDims.y + hallLength)*0.5f * Vector2.up, transform.rotation, tileGrid);
-        else Instantiate(hallsLR[0], roomPos + (roomDims.x + hallLength)*0.5f * Vector2.left, transform.rotation, tileGrid);
 
+        // spawn hallway + update currentPos
+        if (previousExit == 0) { // up
+            Instantiate(hallsUD[0], currentPos + (roomDims.y + hallLength)*0.5f * Vector2.up, transform.rotation, tileGrid);
+            currentPos += Vector2.up * (roomDims.y + hallLength);
+        }
+        else if (previousExit == 1) { // down
+            Instantiate(hallsUD[0], currentPos - (roomDims.y + hallLength)*0.5f * Vector2.up, transform.rotation, tileGrid);
+            currentPos -= Vector2.up * (roomDims.y + hallLength);
+        }
+        else { // right
+            Instantiate(hallsLR[0], currentPos + (roomDims.x + hallLength)*0.5f * Vector2.right, transform.rotation, tileGrid);
+            currentPos += Vector2.right * (roomDims.x + hallLength);
+        }
+        
+        // select room type randomly from all possible continuations
         int type = exitMapping[previousExit][RNGController.MapRNG(0, exitMapping[previousExit].Length)];
-        // Debug.Log("type: " + type);
         GameObject room = rooms[type][RNGController.MapRNG(0, rooms[type].Length)];
-        Instantiate(room, roomPos, transform.rotation, tileGrid);
+        Instantiate(room, currentPos, transform.rotation, tileGrid);
 
+        // set new previousExit for next iteration
+        if (type == 0) previousExit = previousExit == 0 ? 0 : 1;
+        else previousExit = roomExits[type];
 
+        // Debug.Log("Type: " + type);
+        // Debug.Log("Exit: " + previousExit);
     }
 }
