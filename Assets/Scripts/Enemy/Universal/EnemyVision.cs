1c2e22d2e7d0660f7bc46d27733a28b1ac3d2fe9
AdminTroller <lauedmondyc@gmail.com>
2026-08-13 06:13:00 +0800

    started some lighting stuff

--- Assets/Scripts/Enemy/Universal/EnemyVision.cs
+++ Assets/Scripts/Enemy/Universal/EnemyVision.cs
@@ -6,6 +6,7 @@ public class EnemyVision : MonoBehaviour
     private RaycastHit2D _ray;
     private Transform _playerTransform;
     [SerializeField] LayerMask _wallMask;
+    private const float CIRCLE_CAST_RADIUS = 0.6f;
 
     private void Start()
     {
@@ -15,7 +16,7 @@ public class EnemyVision : MonoBehaviour
     private void Update()
     {
         Debug.DrawRay(transform.position, _playerTransform.position - transform.position, Color.red);
-        _ray = Physics2D.CircleCast(transform.position, 0.5f, _playerTransform.position - transform.position, Vector2.Distance(transform.position, _playerTransform.position), _wallMask);
+        _ray = Physics2D.CircleCast(transform.position, CIRCLE_CAST_RADIUS, _playerTransform.position - transform.position, Vector2.Distance(transform.position, _playerTransform.position) - CIRCLE_CAST_RADIUS, _wallMask);
         CanSeePlayer = !_ray.collider;
     }
 }
